9846a3d2eb736ca85b6538356c03afaf34b3f408
Rush <ruspatel@gmail.com>
2019-02-06 13:56:54 -0700

    Second Commit, Updated and created few files.

---
+++ complex_calculator.h
@@ -0,0 +1,19 @@
+//
+//  complex_calculator.hpp
+//  Quantum Computing Project
+//
+//
+
+#ifndef complex_calculator_h
+#define complex_calculator_h
+
+#include <stdio.h>
+#include <complex>
+
+class ComplexCalculations
+{
+public:
+private:
+};
+
+#endif /* complex_calculator_h */
---
+++ quantum_gates.h
@@ -0,0 +1,21 @@
+//
+//  quantum_gates.hpp
+//  Quantum Computing Project
+//
+//  Created by Rush on 2019-02-06.
+//  Copyright © 2019 Rush. All rights reserved.
+//
+
+#ifndef quantum_gates_h
+#define quantum_gates_h
+
+#include <stdio.h>
+#include <complex>
+
+class QuantumGates
+{
+public:
+private:
+};
+
+#endif /* quantum_gates_h */
