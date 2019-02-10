bd5b93d7be09f15fc958411cf824d1c2ce3d1ab0
RusPatel1996 <ruspatel1996@gmail.com>
2019-02-09 23:49:24 -0700

    Update complex_vector_calculator.cpp

--- complex_vector_calculator.cpp
+++ complex_vector_calculator.cpp
@@ -5,3 +5,31 @@
 //
 
 #include "complex_vector_calculator.h"
+
+vector<complex<double>> ComplexVectorCalculations::addition(vector<complex<double>>  complexVector1, vector<complex<double>> complexVector2){
+    return NULL;
+}
+
+vector<complex<double>> ComplexVectorCalculations::subtraction(vector<complex<double>>, vector<complex<double>>){
+    return NULL;
+}
+
+vector<complex<double>> ComplexVectorCalculations::multiplication(vector<complex<double>>, vector<complex<double>>){
+    return 0;
+}
+
+vector<complex<double>> ComplexVectorCalculations::division(vector<complex<double>>, vector<complex<double>>){
+    return 0;
+}
+
+vector<complex<double>> ComplexVectorCalculations::conjugate(vector<complex<double>>, vector<complex<double>>){
+    return 0;
+}
+
+vector<complex<double>> ComplexVectorCalculations::transpose(vector<complex<double>>, vector<complex<double>>){
+    return 0;
+}
+
+vector<complex<double>> ComplexVectorCalculations::adjoint(vector<complex<double>>, vector<complex<double>>){
+    return 0;
+}
