48e8f866f606bbc93a2228510a29d56fdfa43d55
RusPatel1996 <ruspatel1996@gmail.com>
2019-02-10 00:48:39 -0700

    Changed complex_vector_calculator.cpp and .h
    changed all vector<complex<double>> to vector<vector<complex<double>>> to support multidimensional complex vectors. Also added a verify method to make sure the sizes of the multidimensional vectors match up (both with themselves and the other).

--- complex_vector_calculator.cpp
+++ complex_vector_calculator.cpp
@@ -6,30 +6,65 @@
 
 #include "complex_vector_calculator.h"
 
-vector<complex<double>> ComplexVectorCalculations::addition(vector<complex<double>>  complexVector1, vector<complex<double>> complexVector2){
-    return NULL;
+vector<vector<complex<double>>> ComplexVectorCalculations::addition(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2){
+    if (verifySize(complexVector1, complexVector2)){
+        
+    }
 }
 
-vector<complex<double>> ComplexVectorCalculations::subtraction(vector<complex<double>>, vector<complex<double>>){
-    return NULL;
+vector<vector<complex<double>>> ComplexVectorCalculations::subtraction(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2){
+    if (verifySize(complexVector1, complexVector2)){
+        
+    }
 }
 
-vector<complex<double>> ComplexVectorCalculations::multiplication(vector<complex<double>>, vector<complex<double>>){
-    return 0;
+
+vector<vector<complex<double>>> ComplexVectorCalculations::multiplication(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2){
+    if (verifySize(complexVector1, complexVector2)){
+        
+    }
 }
 
-vector<complex<double>> ComplexVectorCalculations::division(vector<complex<double>>, vector<complex<double>>){
-    return 0;
+vector<vector<complex<double>>> ComplexVectorCalculations::division(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2){
+    if (verifySize(complexVector1, complexVector2)){
+        
+    }
 }
 
-vector<complex<double>> ComplexVectorCalculations::conjugate(vector<complex<double>>, vector<complex<double>>){
-    return 0;
+
+vector<vector<complex<double>>> ComplexVectorCalculations::conjugate(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2){
+    if (verifySize(complexVector1, complexVector2)){
+        
+    }
 }
 
-vector<complex<double>> ComplexVectorCalculations::transpose(vector<complex<double>>, vector<complex<double>>){
-    return 0;
+
+vector<vector<complex<double>>> ComplexVectorCalculations::transpose(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2){
+    if (verifySize(complexVector1, complexVector2)){
+        
+    }
 }
 
-vector<complex<double>> ComplexVectorCalculations::adjoint(vector<complex<double>>, vector<complex<double>>){
-    return 0;
+
+vector<vector<complex<double>>> ComplexVectorCalculations::adjoint(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2){
+    if (verifySize(complexVector1, complexVector2)){
+        
+    }
+}
+
+
+bool ComplexVectorCalculations::verifySize(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2){
+    int numOfColumns = (int)complexVector1[0].size();
+    
+    if (complexVector1.size() == complexVector2.size()){
+        for (int row = 0; row <= complexVector1.size(); row++){
+            if ((complexVector1[row].size() != numOfColumns) && (complexVector2[row].size() != numOfColumns)){
+                return false;
+            }
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
 }
--- complex_vector_calculator.h
+++ complex_vector_calculator.h
@@ -14,13 +14,14 @@
 class ComplexVectorCalculations
 {
 public:
-    vector<complex<double>> addition(vector<complex<double>>, vector<complex<double>>);
-    vector<complex<double>> subtraction(vector<complex<double>>, vector<complex<double>>);
-    vector<complex<double>> multiplication(vector<complex<double>>, vector<complex<double>>);
-    vector<complex<double>> division(vector<complex<double>>, vector<complex<double>>);
-    vector<complex<double>> conjugate(vector<complex<double>>, vector<complex<double>>);
-    vector<complex<double>> transpose(vector<complex<double>>, vector<complex<double>>);
-    vector<complex<double>> adjoint(vector<complex<double>>, vector<complex<double>>);
+    vector<vector<complex<double>>> addition(vector<vector<complex<double>>>, vector<vector<complex<double>>>);
+    vector<vector<complex<double>>> subtraction(vector<vector<complex<double>>>, vector<vector<complex<double>>>);
+    vector<vector<complex<double>>> multiplication(vector<vector<complex<double>>>, vector<vector<complex<double>>>);
+    vector<vector<complex<double>>> division(vector<vector<complex<double>>>, vector<vector<complex<double>>>);
+    vector<vector<complex<double>>> conjugate(vector<vector<complex<double>>>, vector<vector<complex<double>>>);
+    vector<vector<complex<double>>> transpose(vector<vector<complex<double>>>, vector<vector<complex<double>>>);
+    vector<vector<complex<double>>> adjoint(vector<vector<complex<double>>>, vector<vector<complex<double>>>);
+    bool verifySize(vector<vector<complex<double>>> complexVector1, vector<vector<complex<double>>> complexVector2);
 protected:
 private:
 };
