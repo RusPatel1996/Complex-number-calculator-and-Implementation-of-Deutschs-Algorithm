9903449273dec22e8459bfe9e7f5e897016d2394
RusPatel1996 <ruspatel1996@gmail.com>
2019-02-14 19:55:03 -0700

    Cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -12,38 +12,6 @@ using namespace std;
 
 int main(){
     
-    vector<vector<complex<double>>> vec1;
-    vector<vector<complex<double>>> vec2;
     
-    vec1.push_back({{3,2},{0,0},{5,-6}});
-    vec1.push_back({{1,0},{4,2},{0,1}});
-    vec1.push_back({{4,-1},{0,0},{4,0}});
-    vec2.push_back({{5,0},{2,-1},{6,-4}});
-    vec2.push_back({{0,0},{4,5},{2,0}});
-    vec2.push_back({{7,-4},{2,7},{0,0}});
-    
-    vector<vector<complex<double>>> vec3;
-    vector<vector<complex<double>>> vec4;
-    vec3.push_back({{1,0},{1,0}});
-    vec3.push_back({{1,0},{1,0}});
-    vec4.push_back({{1,0},{1,0},{1,0}});
-    vec4.push_back({{1,0},{2,0},{1,0}});
-    vec4.push_back({{1,0},{1,0},{1,0}});
-    
-    ComplexVectorCalculations calculate;
-    QuantumGates gate;
-    
-    calculate.viewComplexVector(gate.toffoliGate(1,1,0));
-    
-    //ComplexVectorCalculations calculate;
-    //calculate.viewComplexVector(calculate.tensor(vec3, vec4));
-    
-    
-    /*
-    ComplexVectorCalculations calculate;
-    calculate.viewComplexVector(vec1);
-    calculate.viewComplexVector(calculate.transpose(vec1));
-    calculate.viewComplexVector(calculate.multiplication(vec1, vec2));
-    */
     return 0;
 }
