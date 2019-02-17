6b5330a45950d3368cebce210dec09de67a56bd6
RusPatel1996 <ruspatel1996@gmail.com>
2019-02-16 20:03:11 -0700

    fredkinGate & Deutsch's algoeithm completed

--- quantum_gates.cpp
+++ quantum_gates.cpp
@@ -34,15 +34,13 @@ vector<vector<complex<double>>> QuantumGates::toffoliGate(int bit1, int bit2, in
     return calc.multiplication(toffoliMatrix, calc.tensor(calc.tensor(qubit1, qubit2), qubit3));
 }
 
-/*
 vector<vector<complex<double>>> QuantumGates::fredkinGate(int bit1, int bit2, int bit3){
     qubit1 = initializeQubit(qubit1, bit1);
     qubit2 = initializeQubit(qubit2, bit2);
     qubit3 = initializeQubit(qubit3, bit3);
  
-    return calc.tensor(calc.tensor(qubit1, calc.multiplication(calc.conjugate(qubit1),qubit2.) xor calc.multiplication(qubit1, qubit3)), calc.multiplication(calc.conjugate(qubit1),qubit3) xor calc.multiplication(qubit1, qubit2)));
+    return calc.multiplication(fredkinMatrix, calc.tensor(calc.tensor(qubit1, qubit2), qubit3));
 }
-*/
 
 vector<vector<complex<double>>> QuantumGates::deutschsAlgorithm(int uf0, int uf1){
     qubit1 = initializeQubit(qubit1, 0);
@@ -51,17 +49,37 @@ vector<vector<complex<double>>> QuantumGates::deutschsAlgorithm(int uf0, int uf1
     vector<vector<complex<double>>> ufMatrix;
     
     if (uf0 == 0 && uf1 == 1){
-        
+        ufMatrix = {
+            {{1,0},{0,0},{0,0},{0,0}},
+            {{0,0},{1,0},{0,0},{0,0}},
+            {{0,0},{0,0},{0,0},{1,0}},
+            {{0,0},{0,0},{1,0},{0,0}}
+        };
     }
     else if (uf0 == 1 && uf1 == 0){
-        
+        ufMatrix = {
+            {{0,0},{1,0},{0,0},{0,0}},
+            {{1,0},{0,0},{0,0},{0,0}},
+            {{0,0},{0,0},{1,0},{0,0}},
+            {{0,0},{0,0},{0,0},{1,0}}
+        };
     }
     else if (uf0 == 0 && uf1 == 0){
-        
+        ufMatrix = {
+            {{1,0},{0,0},{0,0},{0,0}},
+            {{0,0},{1,0},{0,0},{0,0}},
+            {{0,0},{0,0},{1,0},{0,0}},
+            {{0,0},{0,0},{0,0},{1,0}}
+        };
     }
     else if (uf0 == 1 && uf1 == 1){
-        
+        ufMatrix = {
+            {{0,0},{1,0},{0,0},{0,0}},
+            {{1,0},{0,0},{0,0},{0,0}},
+            {{0,0},{0,0},{0,0},{1,0}},
+            {{0,0},{0,0},{1,0},{0,0}}
+        };
     }
     
-    return calc.multiplication(calc.multiplication(calc.tensor(qubit1, qubit2), calc.tensor(HadamardMatrix, HadamardMatrix)), calc.tensor(HadamardMatrix, identityMatrix));
+    return calc.multiplication(calc.multiplication(calc.tensor(HadamardMatrix, identityMatrix), ufMatrix), calc.multiplication(calc.tensor(HadamardMatrix, HadamardMatrix), calc.tensor(qubit1, qubit2)));
 }
